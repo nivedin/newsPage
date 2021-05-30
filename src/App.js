06cbd1b23fe83e8e85325553b319fe54a7039809
Nivedin <nivedinp@gmail.com>
2021-05-31 00:29:27 +0530

    changed api to ny-times 2

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import Article from './components/Article';
 function App() {
   const [articles, setArticles] = useState("");
   //newapi//REACT_APP_API_KEY = dc668f6769f5419388409d3b8cff9b1b
+  //NYnewapi//REACT_APP_API_KEY = dOHHYL3jcwhwDaSgGwgtkUiH0pHDr3Nt
   useEffect(() => {
-    axios.get(`https://api.nytimes.com/svc/topstories/v2/upshot.json?api-key=${process.env.REACT_APP_API_KEY}`).then(response => {
+    axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=crimes&limit=50&api-key=${process.env.REACT_APP_API_KEY}`).then(response => {
       console.log(response);
-      setArticles(response.data.results)
+      setArticles(response.data.response.docs)
     }).catch(error => {
       console.log(error);
     })
+    // axios.get(`https://api.nytimes.com/svc/topstories/v2/upshot.json?api-key=${process.env.REACT_APP_API_KEY}`).then(response => {
+    //   console.log(response);
+    //   setArticles(response.data.results)
+    // }).catch(error => {
+    //   console.log(error);
+    // })
     // axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=${process.env.REACT_APP_API_KEY}`).then(response => {
     //   setArticles(response.data.articles)
     // }).catch(error => {
@@ -34,12 +41,19 @@ function App() {
         {articles ? (
           articles?.map((article, i) => (
             <Article key={i}
+              title={article.headline.main}
+              description={article.lead_paragraph}
+              image={article.multimedia[1]?.url}
+              link={article.web_url}
+              publishedAt={article.pub_date}
+            />
+            /* <Article key={i}
               title={article.title}
               description={article.description}
               image={article.multimedia[0].url}
               link={article.url}
               publishedAt={article.created_date}
-            />
+            /> */
           ))
         ) : null}
       </div>
--- src/components/Article.js
+++ src/components/Article.js
@@ -4,7 +4,7 @@ function Article({ title, link, description, image, publishedAt }) {
       <a href={link}>
         <h1>{title}</h1>
         <small>{new Date(publishedAt).toUTCString()}</small>
-        <img src={image} alt="img" />
+        <img src={`http://www.nytimes.com/${image}`} alt="img" />
         <p className="description">{description}</p>
       </a>
     </article>
