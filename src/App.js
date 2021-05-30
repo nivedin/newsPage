8b73a9e016093a2918d6722a5ddb1d4c49673f95
Nivedin <nivedinp@gmail.com>
2021-05-31 00:08:03 +0530

    changed api to ny-times

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Article from './components/Article';
 
 function App() {
   const [articles, setArticles] = useState("");
-
+  //newapi//REACT_APP_API_KEY = dc668f6769f5419388409d3b8cff9b1b
   useEffect(() => {
-    axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=${process.env.REACT_APP_API_KEY}`).then(response => {
-      setArticles(response.data.articles)
+    axios.get(`https://api.nytimes.com/svc/topstories/v2/upshot.json?api-key=${process.env.REACT_APP_API_KEY}`).then(response => {
+      console.log(response);
+      setArticles(response.data.results)
     }).catch(error => {
       console.log(error);
     })
+    // axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=${process.env.REACT_APP_API_KEY}`).then(response => {
+    //   setArticles(response.data.articles)
+    // }).catch(error => {
+    //   console.log(error);
+    // })
   }, [])
 
   console.log(articles);
@@ -30,9 +36,9 @@ function App() {
             <Article key={i}
               title={article.title}
               description={article.description}
-              image={article.urlToImage}
+              image={article.multimedia[0].url}
               link={article.url}
-              publishedAt={article.publishedAt}
+              publishedAt={article.created_date}
             />
           ))
         ) : null}
